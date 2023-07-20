4d86925ef34efe1767047e0973b2f746bbb77345
Terryb <terrydagbartels@gmail.com>
2023-07-20 20:08:05 +0000

    add navLink component

---
+++ src/components/navLink.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link, ResponsiveValue } from '@chakra-ui/react'
+
+interface NavLinkProps {
+  linkText: string;
+  path: string
+  bgColor?: string
+  fontSize?: string
+  color?: string
+  fontWeight?: string
+  ml?: number
+  tAlign?: string
+  w?: string
+  weight?: string
+  fSize?: string
+  bRadius?: string
+}
+
+const NavLink: React.FC<NavLinkProps> = (props) => {
+  const {linkText, fSize, color, path, tAlign, w, bgColor, weight, bRadius } = props
+  return (
+      <Link href={path}
+          pl={5}
+          backgroundColor= {bgColor}
+          color= {color ? color : '#110020'}
+          borderRadius={bRadius}
+          fontSize={fSize}
+          fontWeight={weight ? weight : 'medium'}
+          ml={5}
+          p={1}
+          w={w}
+          textAlign={tAlign ? tAlign : 'left' }
+    >{ linkText }</Link>
+  )
+}
+
+export default NavLink

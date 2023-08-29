01fa919b458f8fb0f58958a6cc059de3fbef338a
Terryb <terrydagbartels@gmail.com>
2023-08-29 11:48:16 +0000

    add nav menu to all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Providers } from './providers'
 import { Inter } from 'next/font/google'
+import Menu from '@/components/nav'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>
+          <Menu />
           {children}
         </Providers>
       </body>
